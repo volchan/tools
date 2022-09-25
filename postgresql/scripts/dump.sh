958231c93ace8bd834dc81b51a2472e2e44297f5
Stephane DE LA TORRE <s.dlt@hotmail.fr>
2022-09-25 10:55:37 +0200

    Update dump script

--- postgresql/scripts/dump.sh
+++ postgresql/scripts/dump.sh
@@ -2,8 +2,9 @@
 
 cd $(dirname "$0")/../backups
 file=$(date +"%Y%m%d%I%M%S").dump.sql
-echo $file
+echo "Dumping $file"
 docker exec -i postgres /usr/bin/pg_dumpall -U postgres > $file
 grep -v "CREATE ROLE postgres;" $file > tmpfile && mv tmpfile $file
 grep -v "ALTER ROLE postgres WITH SUPERUSER INHERIT CREATEROLE CREATEDB LOGIN REPLICATION BYPASSRLS;" $file > tmpfile && mv tmpfile $file
+echo "Dumped $file"
 cd -
